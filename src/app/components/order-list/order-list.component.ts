f0071b733d5626fb16dee987b98827a447de4e61
Mathieu Maisonnette <math.poke@gmail.com>
2023-01-19 22:25:56 +0100

    Expanded Rows for Order List

--- src/app/components/order-list/order-list.component.ts
+++ src/app/components/order-list/order-list.component.ts
@@ -5,11 +5,19 @@ import {MatTable} from '@angular/material/table';
 import {OrderListDataSource} from './order-list-datasource';
 import {Order} from "../../models/order";
 import {OrderService} from "../../services/order.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
     selector: 'app-order-list',
     templateUrl: './order-list.component.html',
-    styleUrls: ['./order-list.component.css']
+    styleUrls: ['./order-list.component.css'],
+    animations: [
+        trigger('detailExpand', [
+            state('collapsed', style({height: '0px', minHeight: '0'})),
+            state('expanded', style({height: '*'})),
+            transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+        ]),
+    ]
 })
 export class OrderListComponent implements OnInit {
     @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -18,6 +26,8 @@ export class OrderListComponent implements OnInit {
 
     dataSource: OrderListDataSource = new OrderListDataSource();
     displayedColumns = ['id', 'ref', 'date', 'price'];
+    columnsToDisplayWithExpand = [...this.displayedColumns, 'expand'];
+    expandedOrder: Order | null | undefined;
 
     constructor(private orderService: OrderService) {
     }
