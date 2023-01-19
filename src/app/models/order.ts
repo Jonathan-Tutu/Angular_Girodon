294a9ed6a17e05aa4568c8302f733d19f928cf71
Mathieu Maisonnette <math.poke@gmail.com>
2023-01-20 00:06:12 +0100

    Order linked to Product

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -1,10 +1,16 @@
 import {User} from "./user";
+import {Product} from "./product";
 
 export interface Order {
     id: number;
     ref: string;
-    products: Array<{id: number, quantity: number}>
+    products: Array<OrderProduct>
     user: User|number
     date: string
     price: number
+}
+
+export interface OrderProduct {
+    product: Product|number;
+    quantity: number;
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -5,30 +5,46 @@ import {Order} from "../models/order";
 import {UserService} from "./user.service";
 import {User} from "../models/user";
 import {map} from "rxjs/operators";
+import {ProductService} from "./product.service";
+import {Product} from "../models/product";
 
 @Injectable({
     providedIn: 'root'
 })
 export class OrderService {
     users?: User[];
+    products?: Product[];
 
     constructor(
         private httpClient: HttpClient,
-        private userService: UserService
+        private userService: UserService,
+        private productService: ProductService
     ) {
     }
 
     public getOrders(): Observable<Order[]> {
         this.userService.getUsers().subscribe((users) => {
             this.users = users;
-        })
+        });
+        this.productService.getProducts().subscribe((products) => {
+            this.products = products;
+        });
+
         return this.httpClient.get<Order[]>("assets/data/order-data.json")
             .pipe(map((orders) => {
+                // Pas très optimisé pour récupérer des données d'autres json mais vu qu'il n'y a pas d'api ni de BDD ^^
                 orders.forEach( (order) => {
                     this.users?.forEach((user) => {
                         if (order.user == user.id) {
                             order.user = user;
                         }
+                    });
+                    order.products.forEach((orderProduct) => {
+                        this.products?.forEach((product) => {
+                            if (orderProduct.product == product.id) {
+                                orderProduct.product = product;
+                            }
+                        });
                     });
                 });
                 return orders;
